b0b53b85d8ad4b1a0d6a8fe603051363462d7e4c
Nathan Mayall <nathanmayall@icloud.com>
2020-10-26 23:33:16 +0000

    Added messages send between users resolver

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require("apollo-server");
 
-const { sequelize } = require('./models');
+const { sequelize } = require("./models");
 
-const resolvers = require('./graphql/resolvers')
-const typeDefs = require('./graphql/typeDefs')
+const resolvers = require("./graphql/resolvers");
+const typeDefs = require("./graphql/typeDefs");
+const contextMiddleware = require("./util/contextMiddleware");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ctx => ctx,
+  context: contextMiddleware,
 });
 
 server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`)
+  console.log(`🚀 Server ready at ${url}`);
 
   sequelize
-  .authenticate()
-  .then(() => console.log('Database Connected!'))
-  .catch((err) => console.log(err))
-})
+    .authenticate()
+    .then(() => console.log("Database Connected!"))
+    .catch((err) => console.log(err));
+});
